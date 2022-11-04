2c66f5507a57bc4d8850145759f1492f9125c4ac
MárioCosta <mariocostadev@gmail.com>
2022-11-04 23:21:41 +0000

    Create 1000.cpp

---
+++ 1000.cpp
@@ -0,0 +1,17 @@
+//timus problem 1000
+
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    
+    ll a, b;
+    
+    scanf("%lld %lld", &a, &b);
+    printf("%lld\n", a+b);   
+    
+    return 0;
+}
