7c64460bfdb5ae7ffb642e22923346135913a26d
MárioCosta <mariocostadev@gmail.com>
2022-11-05 22:23:54 +0000

    Add files via upload

---
+++ Codeforce/71A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+
+  int n;
+  scanf ("%d", &n);
+
+  char s[100];
+  std::vector < string > resposta;
+  for (int x = 0; x < n; x++)
+    {
+      scanf ("%s", s);
+      if (strlen (s) > 10)
+	{
+	  int tmp = strlen (s);
+	  printf ("%c%d%c\n", s[0], tmp - 2, s[tmp - 1]);
+	}
+      else
+	{
+	  printf ("%s\n", s);
+	}
+    }
+
+
+
+  return 0;
+}
